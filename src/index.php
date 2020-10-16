135487eea0a918c5b56938b6fc263be637b6be94
Eleonora <eleonora@MBP-di-Eleonora.station>
2020-10-16 11:31:29 +0200

    added header section

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Landing Page Responsive with Dynamic contents";?></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!--header-->
+
+<div class="container">
+    <div class="header">
+        
+    </div>
+</div>
+    
+</body>
+</html>
